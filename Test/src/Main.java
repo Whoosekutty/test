3347d4ae697c30e77b030ee2e95e4a629ead500a
User <User@DESKTOP-I19M0QE>
2021-03-18 14:58:28 +0530

    file up

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -1,16 +1,22 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Map;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		System.out.println("working");
 		
 		Map<String, String> env = System.getenv();
-		
+		File file = new File("./hello.txt");
+		FileWriter w = new FileWriter(file);
 		for(String s : env.keySet()) {
 			System.out.println(s);
 			System.out.println(env.get(s));
+			w.append(s);
+			w.append(env.get(s));
 		}
 
 	}
