ba1c5e7785935f859971f04659e11250c2b5f06d
User <User@DESKTOP-I19M0QE>
2021-03-19 12:19:29 +0530

    d

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -16,8 +16,8 @@ public class Main {
 		String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
 		System.out.println(timeStamp);
 		File file = new File("reports/hello_"+timeStamp+".txt");
-		File xl = new File("excel"+timeStamp+".xls");
-		File ex = new File("auto"+timeStamp+".xls");
+		File xl = new File("reports/excel"+timeStamp+".xls");
+		File ex = new File("reports/auto"+timeStamp+".xls");
 		
 		FileWriter w = new FileWriter(file);
 		FileWriter xxl = new FileWriter(xl);
