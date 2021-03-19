63e3bea4ce4b14cfd905270c74634e686bbb9350
User <User@DESKTOP-I19M0QE>
2021-03-19 12:54:24 +0530

    b v

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -18,7 +18,7 @@ public class Main {
 		new File("./reports/").mkdirs();
 		File file = new File("./reports/hello_"+timeStamp+".txt");
 		File xl = new File("./reports/excel"+timeStamp+".xls");
-		File ex = new File("./reports/auto"+timeStamp+".txt");
+		File ex = new File("./reports/auto"+timeStamp+".xls");
 		
 		FileWriter w = new FileWriter(file);
 		FileWriter xxl = new FileWriter(xl);
